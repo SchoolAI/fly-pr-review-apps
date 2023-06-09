037565d30d255b982c46c408dfeb1f544f1a7b73
Cahlan Sharp <cahlan@gmail.com>
2023-06-09 15:42:12 -0600

    no copy config

--- entrypoint.sh
+++ entrypoint.sh
@@ -50,7 +50,7 @@ fi
 
 # Deploy the Fly app, creating it first if needed.
 if ! flyctl status --app "$app"; then
-  flyctl launch --verbose --no-deploy --copy-config --name "$app" --region "$region" --org "$org"
+  flyctl launch --verbose --no-deploy --name "$app" --region "$region" --org "$org"
   if [ -n "$INPUT_SECRETS" ]; then
     echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$app"
   fi
